27739b345fe077dafa4f179793106bf50085f4e2
Dario <dariosaric40@gmail.com>
2024-02-13 08:56:03 +0100

    feat: add password to MeetingRoom

--- src/database/entities/meetingRoom.entity.ts
+++ src/database/entities/meetingRoom.entity.ts
@@ -1,43 +1,70 @@
-import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import BaseEntity from "./base.entity";
 import { User } from "./user.entity";
 import { Project } from "./project.entity";
+import { Exclude, instanceToPlain } from "class-transformer";
+import bcrypt from "bcryptjs";
 
 @Entity({
-  name: 'meeting_rooms'
+  name: "meeting_rooms",
 })
 export class MeetingRoom extends BaseEntity {
-  @Column({ name: 'room_name' })
+  @Column({ name: "room_name" })
   public roomName: string;
 
-  @Column({ name: 'room_uid' })
-  public roomUid: string
+  @Column({ name: "room_uid" })
+  public roomUid: string;
+
+  @Column({
+    name: "password",
+    nullable: true,
+  })
+  @Exclude()
+  password: string;
 
   @ManyToOne(() => User, (user) => user.notes)
-  @JoinColumn({ name: 'user_id' })
+  @JoinColumn({ name: "user_id" })
   public user: User;
 
-  @Column({ name: 'user_id' })
+  @Column({ name: "user_id" })
   public userId: number;
 
   @ManyToOne(() => Project, (project) => project.meetingRooms)
-  @JoinColumn({ name: 'project_id' })
+  @JoinColumn({ name: "project_id" })
   public project: Project;
 
-  @Column({ name: 'project_id' })
+  @Column({ name: "project_id" })
   public projectId: number;
 
   @Column({
-    name: 'start_at',
+    name: "start_at",
     type: "timestamp with time zone",
-    nullable: true // null if it is an instant meeting
+    nullable: true, // null if it is an instant meeting
   })
   public startAt: string;
 
   @Column({
-    name: 'end_at',
+    name: "end_at",
     type: "timestamp with time zone",
-    nullable: true
+    nullable: true,
   })
   public endAt: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, 12);
+    }
+  }
+
+  static async comparePasswords(
+    candidatePassword: string,
+    hashedPassword: string
+  ) {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
+  }
+
+  toJSON() {
+    return instanceToPlain(this);
+  }
 }
